61737cabe4c1461f18c68b52841fb78731c52d6f
Abel Gideon <abelgdtk.coding@gmail.com>
2023-07-03 12:22:12 +0300

    Add part1 solutions

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,67 @@
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header title={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>    
+  )
+}
+
+const Header = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>{props.title}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <Part part1={props.parts[0].name} part2={props.parts[0].exercises} />
+      <Part part1={props.parts[1].name} part2={props.parts[1].exercises} />
+      <Part part1={props.parts[2].name} part2={props.parts[2].exercises} />
+    </div>
+  )
+}
+const Part = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        {props.part1} {props.part2}
+      </p>
+    </div>
+  )
+}
+const Total = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    </div>
+  )
+}
+
+export default App
